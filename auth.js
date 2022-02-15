237f905d3af5b2e35b7cb025aacaee650169c541
YinYang117 <slcryan2010@yahoo.com>
2022-02-14 17:00:33 -0700

    working on session stores

--- auth.js
+++ auth.js
@@ -1,8 +1,49 @@
 const db = require('./db/models');
 
-//login
-//logout
-//restore
-//requireAuth
+const loginUser = (req, res, user) => {
+  req.session.auth = {
+    userId: user.id,
+  };
+};
 
-module.exports = {};
+const logoutUser = (req, res) => {
+  delete req.session.auth;
+};
+
+const restoreUser = async (req, res, next) => {
+  // Log the session object to the console
+  // to assist with debugging.
+  console.log(req.session);
+
+  if (req.session.auth) {
+    const { userId } = req.session.auth;
+
+    try {
+      const user = await db.User.findByPk(userId);
+
+      if (user) {
+        res.locals.authenticated = true;
+        res.locals.user = user;
+        next();
+      }
+    } catch (err) {
+      res.locals.authenticated = false;
+      next(err);
+    }
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+};
+
+const requireAuth = (req, res) => {
+  if (!res.locals.authenticated) return res.redirect('/user/login');
+  return next();
+};
+
+module.exports = {
+  loginUser,
+  logoutUser,
+  restoreUser,
+  requireAuth
+};
--- commands.md
+++ commands.md
@@ -1,4 +1,5 @@
 npm i
+npm install connect-pg-simple
 
 psql
 CREATE USER remember_the_ring_app WITH ENCRYPTED PASSWORD '!1Aa654321' CREATEDB;
@@ -8,14 +9,8 @@ npx dotenv sequelize db:create
 npx sequelize model:generate --name User --attributes firstName:string,lastName:string,email:string,hashedPassword:string,currentLevel:integer
 
 npx sequelize model:generate --name List --attributes title:string,userId:integer,categoryId:integer
-
 npx sequelize model:generate --name Task --attributes title:string,description:string,experienceReward:integer,completed:boolean,listId:integer,userId:integer,dueDate:string,dueTime:string
-
-
 npx sequelize model:generate --name Category --attributes name:string
-
 npx dotenv sequelize db:migrate
 
-4220306
-4220909
-421210
+psql -U remember_the_ring_app remember_the_ring < node_modules/connect-pg-simple/table.sql
