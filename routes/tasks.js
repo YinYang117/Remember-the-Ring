f20ac4ac96ff354c12c232be00885b1777faffa8
Chris Britton <chrisbritton790@gmail.com>
2022-02-18 10:48:50 -0800

    added put route handler

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,4 +9,31 @@ router.delete('/:taskId(\\d+)', asyncHandler(async (req, res) => {
     await doomedTask.destroy();
 }));
 
+router.put('/:taskId(\\d+)', asyncHandler(async (req, res) => {
+    const { title, description, experienceReward, dueDate, dueTime } = req.body;
+    const taskId = parseInt(req.params.taskId, 10);
+    const updatedTask = await Task.findByPk(taskId);
+
+    if (title) {
+        updatedTask.title = title;
+    }
+
+    if (description) {
+        updatedTask.description = description;
+    }
+
+    if (experienceReward) {
+        updatedTask.experienceReward = experienceReward;
+    }
+
+    if (dueDate) {
+        updatedTask.dueDate = dueDate;
+    }
+
+    if (dueTime) {
+        updatedTask.dueTime = dueTime;
+    }
+    await updatedTask.update()
+}));
+
 module.exports = router;
